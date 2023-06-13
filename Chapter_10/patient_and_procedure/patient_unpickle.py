2abfeac73372f048baa5ffb46a69bbe4dec1b554
porte <70mr.mystory@gmail.com>
2023-06-13 14:41:49 +0300

    in proccess

--- Chapter_10/patient_and_procedure/patient_unpickle.py
+++ Chapter_10/patient_and_procedure/patient_unpickle.py
@@ -10,25 +10,25 @@ def main():
 
     while not end_of_file:
         try:
-            patient = pickle.load(input_file)
-            desplay_data(patient)
+            pat_data = pickle.load(input_file)
+            desplay_data(pat_data)
         except EOFError:
             end_of_file = True
 
     input_file.close()
 
 
-def desplay_data(patient):
-    print(type(patient))
-    print(f'ФИО: {patient.get_full_name()}')
-    print(f'Адрес, город, область и почтовый индекс: {patient.get_full_address()}')
-    print(f'Телефонный номер: {patient.get_phone()}')
-    print(f'Имя и телефон контактного лица для экстренной связи: {patient.get_contact_phone()}')
+def desplay_data(pat_data):
+    print(type(pat_data))
+    print(f'ФИО: {pat_data.get_full_name()}')
+    print(f'Адрес, город, область и почтовый индекс: {pat_data.get_full_address()}')
+    print(f'Телефонный номер: {pat_data.get_phone()}')
+    print(f'Имя и телефон контактного лица для экстренной связи: {pat_data.get_contact_phone()}')
     print('----------------------------------------------------')
-    print(f'Название процедуры: {class_patient_procedure.Procedure.get_name_procedure()}')
-    print(f'Дата процедуры: {class_patient_procedure.Procedure.get_procedure_date()}')
-    print(f'Имя врача, который выполнял процедуру: {class_patient_procedure.Procedure.get_doctor_name()}')
-    print(f'Стоимость процедуры: {class_patient_procedure.Procedure.get_cost_procedure()}')
+    # print(f'Название процедуры: {pat_data.}')
+    # print(f'Дата процедуры: {class_patient_procedure.Procedure.get_procedure_date(self=)}')
+    # print(f'Имя врача, который выполнял процедуру: {class_patient_procedure.Procedure.get_doctor_name(self=)}')
+    # print(f'Стоимость процедуры: {class_patient_procedure.Procedure.get_cost_procedure(self=)}')
 
 
 if __name__ == '__main__':
